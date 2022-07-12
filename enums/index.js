abaf9b1eb9a498187ee0602c3a6a4d4a058c9ed3
Kavi <ykavi@ebay.com>
2022-07-12 16:52:00 +0300

    added panel icons obj

--- enums/index.js
+++ enums/index.js
@@ -1,5 +1,24 @@
+import { PieChartOutlined } from '@ant-design/icons';
+
 export const URL = 'http://46.101.203.185:3000/api/v1';
 
 export const END_POINTS = {
   GROUPS: 'groups',
+};
+
+export const PANEL_MENU_ICONS = {
+  settings: <PieChartOutlined />,
+  announcements: <PieChartOutlined />,
+  privatebot: <PieChartOutlined />,
+  channelrecords: <PieChartOutlined />,
+  basicsettings: <PieChartOutlined />,
+  warning: <PieChartOutlined />,
+  principles: <PieChartOutlined />,
+  advancedsettings: <PieChartOutlined />,
+  welcome: <PieChartOutlined />,
+  message: <PieChartOutlined />,
+  newmembers: <PieChartOutlined />,
+  newmembersmentions: <PieChartOutlined />,
+  warn: <PieChartOutlined />,
+  muted: <PieChartOutlined />,
 };
