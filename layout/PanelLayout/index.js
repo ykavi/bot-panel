639ad7828e396e695965d6ce901d2b85f331c6a7
Kavi <ykavi@ebay.com>
2022-07-12 20:46:28 +0300

    added manu icons key

--- layout/PanelLayout/index.js
+++ layout/PanelLayout/index.js
@@ -2,6 +2,7 @@ import { DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutl
 import { Breadcrumb, Layout, Menu } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { useGetFetch } from '@hooks';
+import { PANEL_MENU_ICONS } from '@enums';
 import { useRouter } from 'next/router';
 import { setGroupSetting } from '../../redux/actions/main';
 import { useDispatch } from 'react-redux';
@@ -38,18 +39,18 @@ const PanelLayout = ({ children }) => {
     let subManagement = [];
 
     groupSetting?.ControlPanel?.forEach((item) => {
-      controlPanelData.push(getItem(item?.Name, item?.URL, <PieChartOutlined />));
+      controlPanelData.push(getItem(item?.Name, item?.URL, PANEL_MENU_ICONS[item?.URL]));
     });
 
     groupSetting?.Management?.forEach((item, index) => {
       if (item?.Fields.length) {
         item?.Fields?.forEach((subItem) => {
-          subManagement.push(getItem(subItem?.Name, subItem?.URL, <PieChartOutlined />));
+          subManagement.push(getItem(subItem?.Name, subItem?.URL, PANEL_MENU_ICONS[subItem?.URL]));
         });
-        management.push(getItem(item?.Name, index, <PieChartOutlined />, subManagement));
+        management.push(getItem(item?.Name, index, PANEL_MENU_ICONS[index], subManagement));
         subManagement = [];
       } else {
-        management.push(getItem(item?.Name, item?.URL, <PieChartOutlined />));
+        management.push(getItem(item?.Name, item?.URL, PANEL_MENU_ICONS[item?.URL]));
       }
     });
     setMenuItem([...controlPanelData, ...management]);
