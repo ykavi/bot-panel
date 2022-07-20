c8c86c47995ae14ae1b9ba863e2491489a6ca9ad
Kavi <ykavi@ebay.com>
2022-07-20 13:05:03 +0300

    added slider for menu

--- layout/PanelLayout/index.js
+++ layout/PanelLayout/index.js
@@ -77,8 +77,21 @@ const PanelLayout = ({ children }) => {
       style={{
         minHeight: '100vh',
       }}
+      hasSider
     >
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'relative',
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
         <div className="logo">LOGO</div>
         <Menu
           theme="dark"
