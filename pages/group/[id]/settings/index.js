eb3bf1c031017f8b80a8332d50894f564d109687
Kavi <ykavi@ebay.com>
2022-07-21 15:22:40 +0300

    added input value

--- pages/group/[id]/settings/index.js
+++ pages/group/[id]/settings/index.js
@@ -1,6 +1,8 @@
 import { withIsPanelPage } from '@hocs';
 import { useGetFetch } from '@hooks';
+import { Input, Button, Col } from 'antd';
 import { MenuItemCard, SelectBox, Switch } from '@components';
+import { useState } from 'react';
 
 const handleOnSelect = (value) => {
   console.log(`selected ${value}`);
@@ -11,8 +13,20 @@ const handleOnChange = (value) => {
 };
 
 const Settings = () => {
+  const [inputValues, setInputValues] = useState({});
   const { data, loading, error } = useGetFetch('group/-1001799280304/settings');
   console.log(data);
+
+  const onChangeInputHandle = (e, name) => {
+    const { value } = e.target;
+    const newValues = { ...inputValues, [name]: value };
+    setInputValues(newValues);
+  };
+
+  const onClickHandle = (inputName) => {
+    const savedInputValue = inputValues[inputName];
+    console.log('onClickHandle', savedInputValue);
+  };
 
   const isSelectBoxType = ({ displayName, description, options, value }) => (
     <>
@@ -30,12 +44,28 @@ const Settings = () => {
     </>
   );
 
+  const isTextBoxType = ({ displayName, description, value, name }) => (
+    <>
+      <MenuItemCard title={displayName} description={description}>
+        <Col lg={12}>
+          <Input.Group compact>
+            <Input style={{ width: 'calc(100% - 200px)' }} defaultValue="https://ant.design" onChange={(e) => onChangeInputHandle(e, name)} />
+            <Button type="primary" onClick={() => onClickHandle(name)}>
+              Kaydet
+            </Button>
+          </Input.Group>
+        </Col>
+      </MenuItemCard>
+    </>
+  );
+
   return (
     <>
       {data?.GruopSettings?.map((item) => (
         <>
           {item.type === 'Listbox' && isSelectBoxType(item)}
           {item.type === 'Switch Button' && isSwitchType(item)}
+          {item.type === 'Textbox' && isTextBoxType(item)}
         </>
       ))}
     </>
