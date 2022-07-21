28785fc2f84cd16983b92c6495eb760d7d985eb8
Kavi <ykavi@ebay.com>
2022-07-21 20:25:27 +0300

    fix main layout bug

--- layout/MainLayout/index.js
+++ layout/MainLayout/index.js
@@ -1,14 +1,18 @@
 import { Header, Footer, Grid } from '@components';
 import { Main } from './style';
 
-const MainLayout = ({ children }) => (
-  <>
-    <Header />
-    <Grid>
-      <Main>{children}</Main>
-    </Grid>
-    <Footer />
-  </>
-);
+const MainLayout = ({ children }) => {
+  if (typeof window !== 'undefined') document.body.style.overflowY = 'auto';
+
+  return (
+    <>
+      <Header />
+      <Grid>
+        <Main>{children}</Main>
+      </Grid>
+      <Footer />
+    </>
+  );
+};
 
 export default MainLayout;
